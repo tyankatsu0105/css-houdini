5ae79751a28ead76ac33b118662b256e5eccdf50
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-12-27 22:30:50 +0900

    fix

--- src/properties.js
+++ src/properties.js
@@ -1,26 +1,26 @@
-if ("registerProperty" in window.CSS) {
-  window.CSS.registerProperty({
+if (window.CSS && CSS.registerProperty) {
+  CSS.registerProperty({
     name: "--extra-underlineNumber",
     syntax: "<number>",
     inherits: false,
     initialValue: 3
   });
 
-  window.CSS.registerProperty({
+  CSS.registerProperty({
     name: "--extra-underlineColor",
     syntax: "<color>",
     inherits: true,
     initialValue: "black"
   });
 
-  window.CSS.registerProperty({
+  CSS.registerProperty({
     name: "--extra-underlineSpread",
     syntax: "<number>",
     inherits: false,
     initialValue: 20
   });
 
-  window.CSS.registerProperty({
+  CSS.registerProperty({
     name: "--extra-underlineWidth",
     syntax: "<number>",
     inherits: false,
