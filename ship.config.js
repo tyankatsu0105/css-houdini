9e295daf156154b1bdab0d4cca58adf9dc8985b6
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-01-01 03:08:36 +0900

    chore: fix publishCommand

--- ship.config.js
+++ ship.config.js
@@ -1,21 +1,5 @@
 const shell = require('shelljs');
 
-
-const getPackagePath = (dir) => {
-  const splittedDir = dir.split('/');
-  const packagePath = [
-    splittedDir[splittedDir.length - 2],
-    splittedDir[splittedDir.length - 1],
-  ].join('/');
-  return { packagePath };
-};
-
-const getDistPackagePath = (packagePath) => {
-  const distPackagePath = `dist/${packagePath}`;
-
-  return { distPackagePath };
-};
-
 module.exports = {
   monorepo: {
     mainVersionFile: 'package.json',
@@ -24,11 +8,10 @@ module.exports = {
     updateDependencies: false,
   },
   buildCommand: () => 'npm run build:all',
-  publishCommand: ({ defaultCommand, tag, dir }) => {
-    const { packagePath } = getPackagePath(dir);
-    const { distPackagePath } = getDistPackagePath(packagePath);
-
-    shell.cd(distPackagePath);
+  publishCommand: ({ defaultCommand, tag }) => {
+    shell.exec('ls -a');
+    shell.cd('./dist');
+    shell.exec('pwa');
 
     return `${defaultCommand} --access public --tag ${tag}`
   }
