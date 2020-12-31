09a80c0ff978b10ac0ebe17cc455595b80b01008
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-01-01 02:26:02 +0900

    chore: add beforePublish

--- ship.config.js
+++ ship.config.js
@@ -1,11 +1,33 @@
+const getPackagePath = (dir) => {
+  const splittedDir = dir.split('/');
+  const packagePath = [
+    splittedDir[splittedDir.length - 2],
+    splittedDir[splittedDir.length - 1],
+  ].join('/');
+  return { packagePath };
+};
+
+const getDistPackagePath = (packagePath) => {
+  const distPackagePath = `../../dist/${packagePath}`;
+
+  return { distPackagePath };
+};
+
 module.exports = {
   monorepo: {
     mainVersionFile: 'package.json',
     packagesToBump: ['packages/*'],
     packagesToPublish: ['packages/*'],
-    updateDependencies: false
+    updateDependencies: false,
   },
   buildCommand: () => 'npm run build:all',
+  beforePublish: ({ exec, dir }) => {
+    const { packagePath } = getPackagePath(dir);
+    const { distPackagePath } = getDistPackagePath(packagePath);
+
+    exec(`rm -rf ./*`);
+    exec(`cp ${distPackagePath} .`);
+  },
   publishCommand: ({ defaultCommand, tag }) =>
     `${defaultCommand} --access public --tag ${tag}`,
 };
