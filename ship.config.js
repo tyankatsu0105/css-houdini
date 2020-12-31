2ea15593c5a04786c656e27ce941c306ef379cf2
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-01-01 03:19:50 +0900

    chore: fix publishCommand

--- ship.config.js
+++ ship.config.js
@@ -1,5 +1,3 @@
-const shell = require('shelljs');
-
 module.exports = {
   monorepo: {
     mainVersionFile: 'package.json',
@@ -9,9 +7,6 @@ module.exports = {
   },
   buildCommand: () => 'npm run build:all',
   publishCommand: ({ defaultCommand, tag }) => {
-    shell.exec('ls -a');
-    shell.cd('dist/packages/css-houdini-ripple');
-
-    return `${defaultCommand} --access public --tag ${tag}`
+    return `cd ./dist ${defaultCommand} --access public --tag ${tag}`
   }
 };
