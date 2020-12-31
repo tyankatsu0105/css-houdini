d044c2de4cb673154f1f5e8ea8d50f25b2c479c9
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-12-31 20:26:12 +0900

    reset

--- website/index.js
+++
@@ -1,7 +0,0 @@
-import worklet from 'raw-loader!../src/worklet';
-
-const workletBlob = URL.createObjectURL(
-  new Blob([worklet], { type: "application/javascript" })
-);
-
-window.CSS.paintWorklet.addModule(workletBlob);
