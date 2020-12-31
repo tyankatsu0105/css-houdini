7b5ad6d84a51f22a9994bf8de52eabff08998159
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-01-01 00:19:38 +0900

    build: add shipjs

---
+++ ship.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  monorepo: {
+    mainVersionFile: 'package.json',
+    packagesToBump: ['packages/*'],
+    packagesToPublish: ['packages/*'],
+    updateDependencies: false
+  },
+};
